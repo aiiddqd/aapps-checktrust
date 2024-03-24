c454600f0e77d00f716d392ca1d8c690d73a04d6
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2024-03-24 15:40:07 +0300

    drafting: Reports and Settings

--- includes/Reports.php
+++ includes/Reports.php
@@ -1,82 +1,91 @@
-<?php 
+<?php
 
 namespace AappsCheckTrust;
 
 final class Reports {
 
-    public static $slug = 'checktrust';
+	public static $slug = 'checktrust';
 
-    public static function init() {
+	public static function init() {
 
-        add_action( 'admin_menu', function() {
-            add_management_page( 'CheckTrust', 'CheckTrust', 'manage_options', static::$slug, function() {
-                ?>
-                <h1>CheckTrust Reports</h1>
-                <?php 
-                self::update_button();
-                self::render();
-            });
-        });
-    }
+		add_action( 'admin_menu', function () {
+			add_management_page( 'CheckTrust', 'CheckTrust', 'manage_options', static::$slug, function () {
+				?>
+				<h1>CheckTrust Reports</h1>
+				<div class="actions">
+					<span>
+						<?php self::update_button(); ?>
+					</span>
+					<span> | </span>
+					<span>
+						<?= sprintf( '<a href="https://checktrust.ru/cabinet">Go to CheckTrust</a>' ); ?>
+					</span>
 
-    public static function update_button(){
-        $url = admin_url('tools.php?page=' . static::$slug);
-        $url = add_query_arg('action', 'update', $url);
+				</div>
+				<?php
+				self::render();
+			} );
+		} );
+	}
 
-        if(isset($_GET['action']) && 'update' == $_GET['action']){
-            do_action('checktrust_cron_event');
-            printf('<a href="%s">Updated</a>', $url);
+	public static function update_button() {
+		$url = admin_url( 'tools.php?page=' . static::$slug );
+		$url = add_query_arg( 'action', 'update', $url );
 
-        } else {
-            printf('<a href="%s">Update</a>', $url);
+		if ( isset ( $_GET['action'] ) && 'update' == $_GET['action'] ) {
+			do_action( 'checktrust_cron_event' );
+			printf( '<a href="%s">Updated</a>', $url );
 
-        }
-    }
-    public static function render(){
+		} else {
+			printf( '<a href="%s">Update</a>', $url );
 
-        $data = get_transient('checktrust_data');
+		}
+	}
+	public static function render() {
 
-        if(empty($data)){
-            echo 'Data is empty! Update page please';
-            do_action('checktrust_cron_event');
-        }
-        if(isset($data['hostLimitsBalance'])){
-            printf('<p>hostLimitsBalance: %s</p>', $data['hostLimitsBalance']);
-        }
+		$data = get_transient( 'checktrust_data' );
 
-        if(!empty($data['websites'])){
-            self::render_websites($data['websites']);
-        }
-    
-        if(!empty($data['urls'])){
-            self::render_urls($data['urls']);
-        }
-    
-        printf('<a href="https://checktrust.ru/cabinet">Go to CheckTrust</a>');
-    }
+		if ( empty ( $data ) ) {
+			echo 'Data is empty! Update page please';
+			do_action( 'checktrust_cron_event' );
+		}
+		if ( isset ( $data['hostLimitsBalance'] ) ) {
+			printf( '<p>hostLimitsBalance: %s</p>', $data['hostLimitsBalance'] );
+		}
 
-    public static function render_urls($data){
-        echo '<h2>urls</h2>';
-        foreach($data as $key => $value){
-            printf('<h3>%s</h3>', $key);
-            echo '<pre>';
-            var_dump($value);
-            echo '</pre>';
-        }
-        
-    }
-    
-    public static function render_websites($data){
-        echo '<h2>websites</h2>';
+		if ( ! empty ( $data['websites'] ) ) {
+			self::render_websites( $data['websites'] );
+		}
 
-        foreach($data as $key => $value){
-            printf('<h3>%s</h3>', $key);
-            echo '<pre>';
-            var_dump($value);
-            echo '</pre>';
-        }
-        
-    }
+		if ( ! empty ( $data['urls'] ) ) {
+			self::render_urls( $data['urls'] );
+		}
+
+
+	}
+
+	public static function render_urls( $data ) {
+		echo '<h2>urls</h2>';
+		foreach ( $data as $key => $value ) {
+			printf( '<h3>%s</h3>', $key );
+			echo '<pre>';
+			var_dump( $value );
+			echo '</pre>';
+		}
+
+	}
+
+	public static function render_websites( $data ) {
+		echo '<h2>websites</h2>';
+
+		foreach ( $data as $key => $value ) {
+			printf( '<h3>%s</h3>', $key );
+			echo '<pre>';
+			var_dump( $value );
+			echo '</pre>';
+		}
+
+	}
 }
 
 Reports::init();
